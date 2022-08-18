a2d57c23ae2dea01765153bb7c7465e2afb2c0dc
Marcus <marcus.petrihen@gmail.com>
2022-08-18 14:37:53 +0300

    get data from resolvers

--- client/src/app/layouts/card/card-characteristics/card-characteristics.component.ts
+++ client/src/app/layouts/card/card-characteristics/card-characteristics.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Data } from "@angular/router";
+import { ProductCharacteristics } from "src/app/shared/interface/interfaces";
 
 @Component({
-  selector: 'app-card-characteristics',
-  templateUrl: './card-characteristics.component.html',
-  styleUrls: ['./card-characteristics.component.scss']
+  selector: "app-card-characteristics",
+  templateUrl: "./card-characteristics.component.html",
+  styleUrls: ["./card-characteristics.component.scss"],
 })
 export class CardCharacteristicsComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    console.log("Start ngOnInit Card-Characteristics");
+
+    this.route.data.subscribe(
+      (responce: Data) => {
+        // console.log(responce["productCharacteristics"]);
+        this.productCharacteristics = responce["productCharacteristics"];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
+  loader: boolean = true;
+
+  productCharacteristics?: ProductCharacteristics;
 }
