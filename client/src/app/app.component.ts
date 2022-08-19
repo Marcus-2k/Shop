6fd9a1105091a9d93f4d2e30470399b070298ead
Marcus <marcus.petrihen@gmail.com>
2022-08-19 22:47:16 +0300

    for fovorite component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "./shared/service/server/auth.service";
 import { RenameTitleService } from "./shared/service/rename-title.service";
+import { RequestUserService } from "./shared/service/server/request-user.service";
+import { OtherDataService } from "./shared/service/other-data.service";
 
 @Component({
   selector: "app-root",
@@ -8,13 +10,27 @@ import { RenameTitleService } from "./shared/service/rename-title.service";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-  constructor(private auth: AuthService) {}
+  constructor(
+    private auth: AuthService,
+    private requestUser: RequestUserService,
+    private otherData: OtherDataService
+  ) {}
 
   ngOnInit(): void {
     console.log("Start ngOnInit APP");
     const potentialToken = localStorage.getItem("auth-token");
+
     if (potentialToken !== null) {
       this.auth.setToken(potentialToken);
+      this.requestUser.getFavorite().subscribe(
+        (responce: { favorite: string[] }) => {
+          console.log(responce);
+          this.otherData.favoriteListUser = responce.favorite;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     }
 
     // this.renameTitle.renameTitleSite("Інтернет-Магазин");
