af469e8e3cf5102d03138443dd43601b0cddab03
Marcus <marcus.petrihen@gmail.com>
2022-08-20 21:05:30 +0300

    delete subscribe to a params

--- client/src/app/layouts/card/card.component.ts
+++ client/src/app/layouts/card/card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
 import { LinkNavigate, Product } from "src/app/shared/interface/interfaces";
 import { CategoryProductService } from "src/app/shared/service/category-product.service";
 import { RenameTitleService } from "src/app/shared/service/rename-title.service";
@@ -12,45 +12,41 @@ import { RenameTitleService } from "src/app/shared/service/rename-title.service"
 export class CardComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
-
     private categoryName: CategoryProductService,
     private renameTitle: RenameTitleService
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) => {
-      this.id = params["id"];
+    this.route.data.subscribe((responce: any) => {
+      console.log("Start ngOnInit Card");
+      // console.log(responce["product"]);
+      this.product = responce["product"];
 
-      this.route.data.subscribe((responce: any) => {
-        console.log("Start ngOnInit Card");
-        // console.log(responce["product"]);
-        this.product = responce["product"];
+      this.renameTitle.renameTitleSite(responce.product.name);
 
-        this.renameTitle.renameTitleSite(responce.product.name);
-        // Bread Crumbs
-        if (responce.product.category.length === 3) {
-          this.levelOne =
-            this.categoryName.categoryList[
-              responce.product.category[0]
-            ].nameListCategory[
-              responce.product.category[1]
-            ].subNameListCategory[
-              responce.product.category[2]
-            ].titleSubNameListCategory;
-          this.levelTwo =
-            this.categoryName.categoryList[
-              responce.product.category[0]
-            ].nameListCategory[responce.product.category[1]].subNameCategory;
-        }
-        this.location = responce.product.name;
-        // Bread Crumbs
-        // Title
-        this.titleProduct = responce.product.name;
-        // Title
-        // Navigation
-        this.navBar(window.location.pathname.split("/")[3]);
-        // Navigation
-      });
+      // Bread Crumbs
+      if (responce.product.category.length === 3) {
+        this.levelOne =
+          this.categoryName.categoryList[
+            responce.product.category[0]
+          ].nameListCategory[responce.product.category[1]].subNameListCategory[
+            responce.product.category[2]
+          ].titleSubNameListCategory;
+        this.levelTwo =
+          this.categoryName.categoryList[
+            responce.product.category[0]
+          ].nameListCategory[responce.product.category[1]].subNameCategory;
+      }
+      this.location = responce.product.name;
+      // Bread Crumbs
+
+      // Title
+      this.titleProduct = responce.product.name;
+      // Title
+
+      // Navigation
+      this.navBar(window.location.pathname.split("/")[3]);
+      // Navigation
     });
   }
 
@@ -86,7 +82,6 @@ export class CardComponent implements OnInit {
   // Navigation ============================================================================================================
   // Product Content =======================================================================================================
   product?: Product;
-  id: string = "";
 
   sidebar: 0 | 1 | 2 = 0; // 0 === none, 1 === big-sidebar, 2 === small-sidebar
   // Product Content =======================================================================================================
