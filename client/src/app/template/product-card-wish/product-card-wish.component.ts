f6ee3d2a1ed0054d3adf50e2426562fee5da6ebe
Marcus <marcus.petrihen@gmail.com>
2022-08-23 21:38:57 +0300

    add hover effect to product item in wish-list

--- client/src/app/template/product-card-wish/product-card-wish.component.ts
+++ client/src/app/template/product-card-wish/product-card-wish.component.ts
@@ -6,14 +6,22 @@ import { Wish, WishChecked } from "src/app/shared/interface/interfaces";
   templateUrl: "./product-card-wish.component.html",
   styleUrls: ["./product-card-wish.component.scss"],
 })
-export class ProductCardWishComponent {
+export class ProductCardWishComponent implements OnInit {
   constructor() {}
+
+  ngOnInit(): void {
+    if (this.productItem) {
+      this.images = this.productItem?.imageSrc;
+    }
+  }
 
   @Input() productItem?: Wish;
 
+  @Output() transferChecked = new EventEmitter();
+
   url_server_folder: string = "http://localhost:5000/";
 
-  @Output() transferChecked = new EventEmitter();
+  images: string[] = []; // two images product
 
   addInList(checked: boolean) {
     if (this.productItem?._id) {
@@ -24,5 +32,21 @@ export class ProductCardWishComponent {
 
       this.transferChecked.emit(wishChecked);
     }
+  }
+
+  hoverEnter(imageLogo: HTMLImageElement, imageSubLogo: HTMLImageElement) {
+    imageLogo.classList.add("display-none");
+    imageLogo.classList.remove("display-block");
+
+    imageSubLogo.classList.add("display-block");
+    imageSubLogo.classList.remove("display-none");
+  }
+
+  hoverLeave(imageLogo: HTMLImageElement, imageSubLogo: HTMLImageElement) {
+    imageLogo.classList.add("display-block");
+    imageLogo.classList.remove("display-none");
+
+    imageSubLogo.classList.add("display-none");
+    imageSubLogo.classList.remove("display-block");
   }
 }
