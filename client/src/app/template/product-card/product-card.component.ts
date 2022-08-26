6f26b1d9ebe514a4bf2957824deee9fd6b5d2037
Marcus <marcus.petrihen@gmail.com>
2022-08-26 15:27:37 +0300

    create product-card

--- client/src/app/template/product-card/product-card.component.ts
+++ client/src/app/template/product-card/product-card.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { Product } from "src/app/shared/interface/interfaces";
 
 @Component({
-  selector: 'app-product-card',
-  templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  selector: "app-product-card",
+  templateUrl: "./product-card.component.html",
+  styleUrls: ["./product-card.component.scss"],
 })
 export class ProductCardComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log("Start ngOnInit Product-Card");
+
+    if (this.productItem) {
+      this.images = this.productItem?.imageSrc;
+    }
   }
 
+  @Input() productItem?: Product;
+
+  url_server_folder: string = "http://localhost:5000/";
+
+  images: string[] = []; // two images product
+
+  hoverEnter(imageLogo: HTMLImageElement, imageSubLogo: HTMLImageElement) {
+    imageLogo.classList.add("display-none");
+    imageLogo.classList.remove("display-block");
+
+    imageSubLogo.classList.add("display-block");
+    imageSubLogo.classList.remove("display-none");
+  }
+
+  hoverLeave(imageLogo: HTMLImageElement, imageSubLogo: HTMLImageElement) {
+    imageLogo.classList.add("display-block");
+    imageLogo.classList.remove("display-none");
+
+    imageSubLogo.classList.add("display-none");
+    imageSubLogo.classList.remove("display-block");
+  }
 }
