4d6a216117cdd3fafd78372eb93f15ef68b2d8d5
Marcus <marcus.petrihen@gmail.com>
2022-10-14 15:41:47 +0300

    redirect to 404, if status code 404 for page product

--- client/src/app/shared/resolver/card.resolver.ts
+++ client/src/app/shared/resolver/card.resolver.ts
@@ -1,22 +1,35 @@
+import { HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import {
   Resolve,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
+  Router,
 } from "@angular/router";
-import { Observable } from "rxjs";
+import { catchError, Observable, throwError } from "rxjs";
 import { ProductCard } from "src/app/shared/interface/interfaces";
 import { RequestCardService } from "src/app/shared/service/server/request-card.service";
 
 @Injectable({ providedIn: "root" })
 export class CardResolver implements Resolve<ProductCard> {
-  constructor(private requestCard: RequestCardService) {}
+  constructor(
+    private requestCard: RequestCardService,
+    private router: Router
+  ) {}
+
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<ProductCard> | Promise<ProductCard> | ProductCard {
     console.log("Start Card Resolver");
 
-    return this.requestCard.getByIdCard(route.params["id"]);
+    return this.requestCard.getByIdCard(route.params["id"]).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          this.router.navigate(["/404"]);
+        }
+        return throwError(error);
+      })
+    );
   }
 }
