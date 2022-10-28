6661470fd3f4a5b4a59d0a0028383e6c6ca5e7fa
Marcus <marcus.petrihen@gmail.com>
2022-10-28 16:29:16 +0300

    add new client url

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(
-  require("cors")({ credentials: true, origin: process.env.CLIENT_URL_VPS })
+  require("cors")({ credentials: true, origin: process.env.CLIENT_URL_DOMEN })
 );
 app.use(cookieParser());
 
