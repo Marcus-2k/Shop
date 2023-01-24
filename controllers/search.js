fb5d416b43c2706208a3025a5f71e674894b57ef
Marcus <marcus.petrihen@gmail.com>
2023-01-25 00:41:40 +0200

    changing the data with which the server responds
    1. if there are widthCharacteristics fields with a value of true
    in the request body,
    then the values of product characteristics will be sent with the data
    to create filters based on them.
    2. the else block which was also removed, because him no used

--- controllers/search.js
+++ controllers/search.js
@@ -31,76 +31,74 @@ module.exports.search = async function (req, res) {
     // 4 = Акція (action)
     // 5 = За рейтингом (grade)
 
-    if (lengthQueryParams === 4) {
-      console.log("IF");
+    count = await Product.find({
+      name: { $regex: search_text, $options: "i" },
+    })
+      .countDocuments({})
+      .exec(); // Counter element in collection
+    maxPage = Math.ceil(count / limit); // Number rounding 3.02 >>> 4
 
-      count = await Product.find({
+    let product = await Product.find({
+      name: { $regex: search_text, $options: "i" },
+      // $regex >> partial keywords, options "i" >> case insensitivity
+    });
+
+    let categoryNoUnique = [];
+    let productCharacteristics = [];
+
+    product.forEach((element) => {
+      categoryNoUnique.push(element.category);
+      productCharacteristics.push(element.characteristics);
+    }); // Category and Characteristics in collection
+
+    // Type sorting ==============================================================
+    if (type_sort === 0) {
+      // Сheap
+      product = await Product.find({
         name: { $regex: search_text, $options: "i" },
       })
-        .countDocuments({})
-        .exec(); // Counter element in collection
-      maxPage = Math.ceil(count / limit); // Number rounding 3.02 >>> 4
-
-      let product = await Product.find({
+        .sort({ price: 1 })
+        .limit(limit)
+        .skip(limit * --page);
+    } else if (type_sort === 1) {
+      // Expensive
+      product = await Product.find({
         name: { $regex: search_text, $options: "i" },
-        // $regex >> partial keywords, options "i" >> case insensitivity
-      });
+      })
+        .sort({ price: -1 })
+        .limit(limit)
+        .skip(limit * --page);
+    } else if (type_sort === 2) {
+      // Popularity <Disabled Client>
+    } else if (type_sort === 3) {
+      // Novelty <Disabled Client>
+    } else if (type_sort === 4) {
+      // Action
+      product = await Product.find({
+        name: { $regex: search_text, $options: "i" },
+      })
+        .sort({ action: -1 })
+        .limit(limit)
+        .skip(limit * --page);
 
-      let categoryNoUnique = [];
-      let productCharacteristics = [];
-
-      product.forEach((element) => {
-        categoryNoUnique.push(element.category);
-        productCharacteristics.push(element.characteristics);
-      }); // Category and Characteristics in collection
-
-      // Type sorting ==============================================================
-      if (type_sort === 0) {
-        // Сheap
-        product = await Product.find({
-          name: { $regex: search_text, $options: "i" },
-        })
-          .sort({ price: 1 })
-          .limit(limit)
-          .skip(limit * --page);
-      } else if (type_sort === 1) {
-        // Expensive
-        product = await Product.find({
-          name: { $regex: search_text, $options: "i" },
-        })
-          .sort({ price: -1 })
-          .limit(limit)
-          .skip(limit * --page);
-      } else if (type_sort === 2) {
-        // Popularity <Disabled Client>
-      } else if (type_sort === 3) {
-        // Novelty <Disabled Client>
-      } else if (type_sort === 4) {
-        // Action
-        product = await Product.find({
-          name: { $regex: search_text, $options: "i" },
-        })
-          .sort({ action: -1 })
-          .limit(limit)
-          .skip(limit * --page);
-
-        // Delete item product if action = true
-        for (let idx = 0; idx < product.length; idx++) {
-          if (product[idx].action === false) {
-            product.splice(idx, 1);
-            idx--;
-          }
+      // Delete item product if action = true
+      for (let idx = 0; idx < product.length; idx++) {
+        if (product[idx].action === false) {
+          product.splice(idx, 1);
+          idx--;
         }
-      } else if (type_sort === 5) {
-        // Grade
-        product = await Product.find({
-          name: { $regex: search_text, $options: "i" },
-        })
-          .limit(limit)
-          .skip(limit * --page);
       }
+    } else if (type_sort === 5) {
+      // Grade
+      product = await Product.find({
+        name: { $regex: search_text, $options: "i" },
+      })
+        .limit(limit)
+        .skip(limit * --page);
+    }
 
-      // ===========================================================================
+    // ===========================================================================
+    if (req.body.widthCharacteristics) {
       const uniqueProductCategory = deleteDuplicateCategory(categoryNoUnique); // Unique Category Product
       // console.log(uniqueProductCategory);
 
@@ -134,147 +132,9 @@ module.exports.search = async function (req, res) {
         maxPage,
         limit,
       });
-    } else if (lengthQueryParams > 3) {
-      console.log("ELSE IF");
-      // ===================================================================================================================
-      // ===================================================================================================================
-      // ===================================================================================================================
-      let parameters = Object.values(req.query).splice(1); // {search_text: "телефон"; ram: "4 ГБ,16 ГБ"} >>> ['4 ГБ,16 ГБ']
-      parameters = parameters.flat(1);
-      parameters.pop(); // delete page
-      parameters.pop(); // delete limit
-
-      const parametersToString = [];
-      parameters.forEach((element, idx) => {
-        element.split(",").forEach((item) => {
-          // parametersToString.push([item]);
-          parametersToString.push(item);
-        });
-      }); // [ '6 ГБ', 'Білий', '1 рік,2 роки' ] >>> [ [ '6 ГБ' ], [ 'Білий' ], [ '1 рік' ], [ '2 роки' ] ]
-
-      // console.log(parametersToString);
-      // ===================================================================================================================
-      let parametersQuery = Object.entries(req.query).splice(1);
-      parametersQuery.pop(); // delete page
-      parametersQuery.pop(); // delete limit
-      parametersQuery = parametersQuery.flat(1);
-
-      const queryParams = {};
-      parametersQuery.forEach((element, idx) => {
-        if (idx % 2 === 0) {
-          idx++;
-          // ===
-          // queryParams[element] = parameters2[idx].split(",");
-          // ===
-          if (parametersQuery[idx].indexOf(",") !== -1) {
-            queryParams[element] = parametersQuery[idx].split(",");
-          } else {
-            queryParams[element] = parametersQuery[idx];
-          }
-          // ===
-        }
-      });
-      // console.log(queryParams);
-      // ===================================================================================================================
-      // ===================================================================================================================
-      // ===================================================================================================================
-
-      count = await Product.find({
-        name: { $regex: search_text, $options: "i" },
-      })
-        .countDocuments({})
-        .exec(); // Counter element in collection
-      maxPage = Math.ceil(count / limit); // Number rounding 3.02 >>> 4
-
-      let product = await Product.find({
-        name: { $regex: search_text, $options: "i" },
-      });
-
-      let categoryNoUnique = [];
-      let productCharacteristics = [];
-
-      product.forEach((element) => {
-        categoryNoUnique.push(element.category);
-        productCharacteristics.push(element.characteristics);
-      }); // Category and Characteristics in collection
-
-      // Type sorting ==============================================================
-      if (type_sort === 0) {
-        // Сheap
-        product = await Product.find({
-          name: { $regex: search_text, $options: "i" },
-        })
-          .sort({ price: 1 })
-          .limit(limit)
-          .skip(limit * --page);
-      } else if (type_sort === 1) {
-        // Expensive
-        product = await Product.find({
-          name: { $regex: search_text, $options: "i" },
-        })
-          .sort({ price: -1 })
-          .limit(limit)
-          .skip(limit * --page);
-      } else if (type_sort === 2) {
-        // Popularity <Disabled Client>
-      } else if (type_sort === 3) {
-        // Novelty <Disabled Client>
-      } else if (type_sort === 4) {
-        // Action
-        product = await Product.find({
-          name: { $regex: search_text, $options: "i" },
-        })
-          .sort({ action: -1 })
-          .limit(limit)
-          .skip(limit * --page);
-
-        // Delete item product if action = true
-        for (let idx = 0; idx < product.length; idx++) {
-          if (product[idx].action === false) {
-            product.splice(idx, 1);
-            idx--;
-          }
-        }
-      } else if (type_sort === 5) {
-        // Grade
-        product = await Product.find({
-          name: { $regex: search_text, $options: "i" },
-        })
-          .limit(limit)
-          .skip(limit * --page);
-      }
-
-      // ===========================================================================
-      const uniqueProductCategory = deleteDuplicateCategory(categoryNoUnique); // Unique Category Product
-      // console.log(uniqueProductCategory);
-
-      const counterProductInCategory = counterProduct(
-        uniqueProductCategory,
-        categoryNoUnique
-      ); // Counter category, and product in it
-      // console.log(counterProductInCategory);
-
-      const productCharacteristicsBlock = productBlock(
-        counterProductInCategory,
-        productCharacteristics
-      ); // Product parameters block by category
-      // console.log(productCharacteristicsBlock);
-      // ===========================================================================
-      let productCharacteristicsName = [];
-      uniqueProductCategory.forEach((element) => {
-        if (element.length === 3) {
-          productCharacteristicsName.push(
-            Catalog.categoryList[element[0]].nameListCategory[element[1]]
-              .subNameListCategory[element[2]].characteristics
-          );
-        }
-      });
-
+    } else {
       return res.status(200).json({
         product,
-        uniqueProductCategory,
-        productCharacteristicsBlock,
-        productCharacteristicsName,
         currentPage,
         maxPage,
         limit,
@@ -285,16 +145,6 @@ module.exports.search = async function (req, res) {
     return res.status(500).json({ message: "Server error" });
   }
 };
-
-// module.exports.searchByQuery = async function (req, res) {
-//   console.log("Server searchByQuery");
-
-//   try {
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).json({ message: "Server error" });
-//   }
-// };
 
 // All function this controller ==================================================================================
 
