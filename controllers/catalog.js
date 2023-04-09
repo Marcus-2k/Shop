cc33e8f03ff8b150010618b85db72553d25ba649
Marcus <marcus.petrihen@gmail.com>
2023-04-09 18:11:06 +0300

    renaming functions according to what they do

--- controllers/catalog.js
+++ controllers/catalog.js
@@ -1,8 +1,8 @@
 const { CATALOG } = require("../db/catalog");
 const { CATEGORY } = require("../db/category");
 
-module.exports.getCategory = async function (req, res) {
-  console.log("Server getCategory");
+module.exports.getCatalog = async function (req, res) {
+  console.log("Server getCatalog");
 
   try {
     return res.status(200).json(CATALOG);
@@ -12,8 +12,8 @@ module.exports.getCategory = async function (req, res) {
   }
 };
 
-module.exports.getCategoryHome = async function (req, res) {
-  console.log("Server getCategoryHome");
+module.exports.getCatalogHome = async function (req, res) {
+  console.log("Server getCatalogHome");
 
   try {
     const CLONE_CATALOG = JSON.parse(JSON.stringify(CATALOG));
@@ -30,47 +30,6 @@ module.exports.getCategoryHome = async function (req, res) {
     });
 
     return res.status(200).json(catalogPopuap);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: "Server error" });
-  }
-};
-
-module.exports.getCategoryAndCharacteristics = async function (req, res) {
-  console.log("Server getCategoryAndCharacteristics");
-
-  try {
-    return res.status(200).json(CATEGORY);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: "Server error" });
-  }
-};
-
-module.exports.getCharacteristics = async function (req, res) {
-  console.log("Server getCharacteristics");
-
-  try {
-    const categoryNumber = req.body.categoryNumber;
-
-    if (!categoryNumber) {
-      return res.status(404).json({ message: "Incorrect data entry" });
-    }
-
-    let characteristics = [];
-    if (categoryNumber.length === 3) {
-      characteristics =
-        CATEGORY[categoryNumber[0]].nameListCategory[categoryNumber[1]]
-          .subNameListCategory[categoryNumber[2]].characteristics;
-    } else if (categoryNumber.length === 2) {
-      characteristics =
-        CATEGORY[categoryNumber[0]].nameListCategory[categoryNumber[1]]
-          .characteristics;
-    } else {
-      return res.status(404).json({ message: "Incorrect data entry" });
-    }
-
-    return res.status(200).json(characteristics);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Server error" });
@@ -106,6 +65,47 @@ module.exports.getCatalogSection = async function (req, res) {
     }
 
     return res.status(404).json({ message: "Не існуючий розділ каталогу" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports.getCategory = async function (req, res) {
+  console.log("Server getCategory");
+
+  try {
+    return res.status(200).json(CATEGORY);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports.getCharacteristics = async function (req, res) {
+  console.log("Server getCharacteristics");
+
+  try {
+    const categoryNumber = req.body.categoryNumber;
+
+    if (!categoryNumber) {
+      return res.status(404).json({ message: "Incorrect data entry" });
+    }
+
+    let characteristics = [];
+    if (categoryNumber.length === 3) {
+      characteristics =
+        CATEGORY[categoryNumber[0]].nameListCategory[categoryNumber[1]]
+          .subNameListCategory[categoryNumber[2]].characteristics;
+    } else if (categoryNumber.length === 2) {
+      characteristics =
+        CATEGORY[categoryNumber[0]].nameListCategory[categoryNumber[1]]
+          .characteristics;
+    } else {
+      return res.status(404).json({ message: "Incorrect data entry" });
+    }
+
+    return res.status(200).json(characteristics);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Server error" });
--- routes/catalog.js
+++ routes/catalog.js
@@ -2,13 +2,11 @@ const express = require("express");
 const controller = require("../controllers/catalog");
 const router = express.Router();
 
-router.get("/category/", controller.getCategory);
-router.get("/category/home", controller.getCategoryHome);
-router.get(
-  "/category/characteristics",
-  controller.getCategoryAndCharacteristics
-);
-router.post("/only-characteristics", controller.getCharacteristics);
+router.get("/category/", controller.getCatalog);
+router.get("/category/home", controller.getCatalogHome);
 router.get("/:navigate_link", controller.getCatalogSection);
+
+router.get("/category/characteristics", controller.getCategory);
+router.post("/only-characteristics", controller.getCharacteristics);
 
 module.exports = router;
