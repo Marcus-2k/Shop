08ecceb26f583ed35b0d1ef6a955ebbdbbccc4f1
Marcus <marcus.petrihen@gmail.com>
2023-04-29 22:53:15 +0300

    create route for guest cart

--- server/routes/guest.js
+++ server/routes/guest.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getHistoryGuest } from "../controllers/guest.js";
+import { getHistoryGuest, getGuestCart } from "../controllers/guest.js";
 
 const router = express.Router();
 
 router.get("/history/", getHistoryGuest);
+router.get("/cart", getGuestCart);
 
 export default router;
