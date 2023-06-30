e32e3ee7a5eda9e59ee5ae4fa15db87149ca7594
Marcus <marcus.petrihen@gmail.com>
2023-06-30 12:38:19 +0300

    connect to mongodb

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from "@nestjs/common";
+
+// Mongo
+import { MongooseModule } from "@nestjs/mongoose";
+
+// Controllers
 import { AppController } from "./app.controller";
+
+// Services
 import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot("mongodb://127.0.0.1:27017/Marketplace")],
   controllers: [AppController],
   providers: [AppService],
 })
