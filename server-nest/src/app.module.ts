e8449ec775d768ffe2890b477f499e74e82329db
Marcus <marcus.petrihen@gmail.com>
2023-06-30 14:21:40 +0300

    deleted app.controller and app.service

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -1,21 +1,17 @@
 import { Module } from "@nestjs/common";
 
+// Module
+import { DatabaseModule } from "./module/database/database.module";
+
 // Mongo
 import { MongooseModule } from "@nestjs/mongoose";
-
-// Controllers
-import { AppController } from "./app.controller";
-
-// Services
-import { AppService } from "./app.service";
-import { DatabaseModule } from "./module/database/database.module";
 
 @Module({
   imports: [
     MongooseModule.forRoot("mongodb://127.0.0.1:27017/Marketplace"),
     DatabaseModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
