e277ed8dbe9a83f5a0ca0401cefbc1ce015b3e5b
Marcus <marcus.petrihen@gmail.com>
2023-07-01 23:33:53 +0300

    authorisation using jwt

--- server-nest/src/module/auth/auth.module.ts
+++ server-nest/src/module/auth/auth.module.ts
@@ -1,7 +1,17 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+
 import { AuthController } from "./auth.controller";
+import { JwtStrategy } from "src/shared/middleware/passport";
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.SECRET_KEY_JWT,
+      signOptions: { expiresIn: "12h" },
+    }),
+  ],
   controllers: [AuthController],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
