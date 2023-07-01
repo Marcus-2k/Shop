6fe08a72583411c08485fd55509090ca6898dc7c
Marcus <marcus.petrihen@gmail.com>
2023-07-01 23:10:53 +0300

    file handler, (photos of products)

---
+++ server-nest/src/shared/middleware/upload.ts
@@ -0,0 +1,36 @@
+import * as multer from "multer";
+import * as moment from "moment";
+import { MulterOptions } from "@nestjs/platform-express/multer/interfaces/multer-options.interface";
+
+const storage: MulterOptions["storage"] = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "uploads/");
+  },
+
+  filename(req, file, cb) {
+    const date = moment().format("DDMMYYYY-HHmmss SSS");
+    cb(null, `${date}-${file.originalname}`);
+  },
+});
+
+const fileFilter: MulterOptions["fileFilter"] = (
+  req: any,
+  file,
+  cb: (error: Error | null, acceptFile: boolean) => void
+): void => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/webp"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const limits: MulterOptions["limits"] = {
+  fileSize: 1024 * 1024 * 5,
+};
+
+export const upload: MulterOptions = { storage, fileFilter, limits };
