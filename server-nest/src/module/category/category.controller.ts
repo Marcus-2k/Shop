b38ec3be4744c20e0aa9b69dd545af46e25875ac
Marcus <marcus.petrihen@gmail.com>
2023-07-07 12:21:14 +0300

    endpoint for get category in category module

--- server-nest/src/module/category/category.controller.ts
+++ server-nest/src/module/category/category.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Res } from "@nestjs/common";
+import { Response } from "express";
+import { CATEGORY } from "src/shared/db/category";
+import { Category } from "src/shared/interfaces/category";
 
 @Controller("category")
-export class CategoryController {}
+export class CategoryController {
+  @Get() async getCategory(
+    @Res() response: Response
+  ): Promise<Response<Category[]>> {
+    return response.status(200).json(CATEGORY);
+  }
+}
