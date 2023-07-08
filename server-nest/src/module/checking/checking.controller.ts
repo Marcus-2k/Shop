02640b188941dc3ba0c15ddb9ccbc7c8b380e28f
Marcus <marcus.petrihen@gmail.com>
2023-07-08 15:59:56 +0300

    endpoint for checking controller

--- server-nest/src/module/checking/checking.controller.ts
+++ server-nest/src/module/checking/checking.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Res } from "@nestjs/common";
+import { Response } from "express";
 
 @Controller("checking")
-export class CheckingController {}
+export class CheckingController {
+  @Get("status-server")
+  async checkingStatusServer(
+    @Res() response: Response<{ status: number }>
+  ): Promise<Response<{ status: number }>> {
+    return response.status(200).json({ status: 200 });
+  }
+}
