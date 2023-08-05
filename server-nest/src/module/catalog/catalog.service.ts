75ba6a8e7241791b8d6c6d86ea8e850123ac577e
Marcus <marcus.petrihen@gmail.com>
2023-08-05 18:59:55 +0300

    widget sectionId

--- server-nest/src/module/catalog/catalog.service.ts
+++ server-nest/src/module/catalog/catalog.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from "@nestjs/common";
 import { CATALOG } from "src/shared/db/catalog";
 import {
-  Catalog,
+  CatalogNameListCategory,
   CatalogSubNameListCategory,
 } from "src/shared/interfaces/catalog";
 import { CategoryNumber } from "src/shared/interfaces/category-number";
 import { Breadcrumbs } from "src/shared/interfaces/widget/breadcrumbs";
+import { CategoryService } from "../category/category.service";
+import { MessageRes } from "src/shared/interfaces/res/message";
 
 @Injectable()
 export class CatalogService {
-  public constructor() {}
+  public constructor(private readonly categoryService: CategoryService) {}
 
   public createWidgetBreadcrumbs(categoryNumber: CategoryNumber): Breadcrumbs {
     const widget: Breadcrumbs = {
@@ -40,12 +42,84 @@ export class CatalogService {
   }
 
   public createWidgetAutoPortal(
-    categoryNumber: CategoryNumber
+    categoryNumber: CategoryNumber,
   ): CatalogSubNameListCategory[] {
     const widget: CatalogSubNameListCategory[] =
       CATALOG[categoryNumber[0]].nameListCategory[categoryNumber[1]]
         .subNameListCategory;
 
     return widget;
+  }
+
+  public createWidgetSectionId(category: string[]): MessageRes | any {
+    const categoryNumber: CategoryNumber[] = [];
+    for (let idx = 0; idx < category.length; idx++) {
+      const result: CategoryNumber | MessageRes =
+        this.categoryService.getCategoryNumberByCategory(category[idx]);
+
+      if (!Array.isArray(result)) {
+        return result;
+      }
+
+      categoryNumber.push(result);
+    }
+
+    const sectionId: CatalogNameListCategory[] = [];
+    for (let idx = 0; idx < categoryNumber.length; idx++) {
+      const element: CategoryNumber = categoryNumber[idx];
+
+      if (element.length === 3) {
+        const section: CatalogNameListCategory = JSON.parse(
+          JSON.stringify(CATALOG[element[0]].nameListCategory[element[1]]),
+        );
+
+        section.subNameListCategory = [
+          JSON.parse(
+            JSON.stringify(
+              CATALOG[element[0]].nameListCategory[element[1]]
+                .subNameListCategory[element[2]],
+            ),
+          ),
+        ];
+
+        let position_duplicate!: number;
+        let find_duplicate: boolean = false;
+        for (let idx = 0; idx < sectionId.length; idx++) {
+          if (sectionId[idx].navigate_link === section.navigate_link) {
+            find_duplicate = true;
+            position_duplicate = Number(idx);
+            break;
+          }
+        }
+
+        if (find_duplicate) {
+          sectionId[position_duplicate].subNameListCategory.push(
+            ...section.subNameListCategory,
+          );
+        } else {
+          sectionId.push(section);
+        }
+      } else {
+        const section: CatalogNameListCategory = JSON.parse(
+          JSON.stringify(CATALOG[element[0]].nameListCategory[element[1]]),
+        );
+
+        delete section.subNameListCategory;
+
+        let find_duplicate: boolean = false;
+        for (let idx = 0; idx < sectionId.length; idx++) {
+          if (sectionId[idx].navigate_link === section.navigate_link) {
+            find_duplicate = true;
+            break;
+          }
+        }
+
+        if (!find_duplicate) {
+          sectionId.push(section);
+        }
+      }
+    }
+
+    return sectionId;
   }
 }
--- server-nest/src/shared/interfaces/catalog.ts
+++ server-nest/src/shared/interfaces/catalog.ts
@@ -2,33 +2,34 @@ export interface Catalog {
   nameCategory: string;
   nameCategoryImg: string;
   navigate_link: string;
-  nameListCategory: (
-    | {
-        subNameCategory: string;
-        navigate_link: string;
-        type: 1;
-        image_link: string;
-        permissionUse: boolean;
-        subNameListCategory: CatalogSubNameListCategory[];
-      }
-    | {
-        subNameCategory: string;
-        navigate_link: string;
-        type: 1;
-        image_link: string;
-        permissionUse: boolean;
-        subNameListCategory?: undefined;
-      }
-    | {
-        subNameCategory: string;
-        navigate_link: string;
-        type: 2;
-        image_link: string;
-        permissionUse: boolean;
-        subNameListCategory: CatalogSubNameListCategory[];
-      }
-  )[];
+  nameListCategory: CatalogNameListCategory[];
 }
+
+export type CatalogNameListCategory =
+  | {
+      subNameCategory: string;
+      navigate_link: string;
+      type: 1;
+      image_link: string;
+      permissionUse: boolean;
+      subNameListCategory: CatalogSubNameListCategory[];
+    }
+  | {
+      subNameCategory: string;
+      navigate_link: string;
+      type: 1;
+      image_link: string;
+      permissionUse: boolean;
+      subNameListCategory?: undefined;
+    }
+  | {
+      subNameCategory: string;
+      navigate_link: string;
+      type: 2;
+      image_link: string;
+      permissionUse: boolean;
+      subNameListCategory: CatalogSubNameListCategory[];
+    };
 
 export type CatalogSubNameListCategory =
   | {
