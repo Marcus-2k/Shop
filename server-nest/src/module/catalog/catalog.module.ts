0f378d24fba215a714dd9dcab79f831afbb96d7f
Marcus <marcus.petrihen@gmail.com>
2023-08-05 19:00:29 +0300

    fix bag import CatalogModule

--- server-nest/src/module/catalog/catalog.module.ts
+++ server-nest/src/module/catalog/catalog.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { CatalogController } from "./catalog.controller";
 import { CatalogService } from "./catalog.service";
+import { CategoryModule } from "../category/category.module";
 
 @Module({
+  imports: [CategoryModule],
   controllers: [CatalogController],
   providers: [CatalogService],
   exports: [CatalogService],
